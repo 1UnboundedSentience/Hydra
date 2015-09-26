d03f221f35de29db63dd4707f769a3d8d4ca541d
1UnboundedSentience <jsnx21@gmail.com>
2015-09-26 12:03:52 -0700

    working devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users
+  root to: "home#index"
+
+  # Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  #   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
